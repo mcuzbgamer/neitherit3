7d561d8a660bb4f7687f2841528e1c89894e1b9f
mcuzbgamer <111974683+mcuzbgamer@users.noreply.github.com>
2022-11-08 17:01:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const mineflayer = require('mineflayer')
 const bot = mineflayer.createBot({
  host:'ir.skyblock.uz',
  port: 25566,
- username: 'NETHERITE'
+ username: 'Neitherminer_3'
 })
 bot.on('messagestr', (message) => {
-  if(message.includes("/log"))
+  if(message.includes("/register"))
   {
-    bot.chat("/login 200506@@@")
+    bot.chat("/reg jobir2009 jobir2009")
   }
   
 
@@ -16,38 +16,44 @@ bot.on('messagestr', (message) => {
 
 
 )
+bot.on('messagestr', (message) => {
+  if(message.includes("/log"))
+  {
+    bot.chat("/login jobir2009")
+  }
+  
 
+}
+
+
+)
 async function dig() {
   if (!bot.heldItem || !bot.heldItem.name.includes('pickaxe')) {
     var pickaxe = bot.inventory.items().filter(i => i.name.includes('pickaxe'))[0];
     if (pickaxe) await bot.equip(pickaxe, 'hand')
     if(!pickaxe) bot.quit();
   }
-  var block = bot.blockAtCursor(6);
+  var block = bot.blockAtCursor(7);
   if (!block) return setTimeout(function() {
     dig();
   }, 100);
   await bot.dig(block, 'ignore', 'raycast')
   dig()
 }
-
-bot.on('chat', (username, message) => {
-  if(username=='MineCrafter') {
-    if(message=='NETHERITE') {
-      dig();
-    }
-  }
+bot.once("spawn", () => {
+    setTimeout(() => {
+        dig();
+   }, 20000); 
 })
-    bot.on('kicked', console.log)
-bot.on('error', console.log)
-
 bot.on('chat', (username, message) => {
-    if (username === 'MineCrafter') {
-    if (message.indexOf('!') !== -1) {
-            var replacement = "!",
+    if (username === 'FamousTrain1733') {
+    if (message.indexOf('@') !== -1) {
+            var replacement = "@",
                 toReplace = "",
                 str = message
 
             str = str.replace(replacement, toReplace)
             bot.chat(str)
         }}})
+    bot.on('kicked', console.log)
+bot.on('error', console.log)
